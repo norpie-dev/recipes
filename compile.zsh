5f7b161fa536cd25a9b2620db16c6da77bd3ddd2
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2022-02-22 11:40:12 +0100

    added remove to compilations

--- compile.zsh
+++ compile.zsh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 [[ $# -ne 1 ]] && echo "provide 1 argument" && exit 1
-[[ ! -f $1 ]] && echo "$1 doesnt exist" && exit 1
+
+[[ ! -f $1 ]] && rm *.pdf && echo "$1 doesnt exist" && exit 1
+
 file=${1:r}
 mkdir -p .cache &&
     pdflatex -jobname=$file -halt-on-error -output-directory=.cache $file.tex &&
